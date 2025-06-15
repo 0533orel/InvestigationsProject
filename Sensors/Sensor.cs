ff987b344d721a04593e3c2912c023686ffa3138
orel <0533orel@gmail.com>
2025-06-15 15:02:40 +0300

    create abstract class Sensor

---
+++ Sensors/Sensor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestigationsProject.Classes
+{
+    public abstract class Sensor
+    {
+        public string Name { get; private set; }
+        public bool IsActive { get; private set; }
+
+
+        public Sensor(string name)
+        {
+            Name = name;
+            IsActive = true;
+        }
+
+
+        public abstract bool Activate();
+        
+    }
+
+
+}
